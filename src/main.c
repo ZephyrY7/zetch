d2f36253edbcc905ed4d7e1d51e3df4db11ebbde
ZephyrY7 <leeenhao2@gmail.com>
2023-06-28 01:14:21 +0800

    reduce macros

--- src/main.c
+++ src/main.c
@@ -3,9 +3,6 @@
 #include "fetches.h"
 
 #define ascii "\x1b[38;2;245;224;220m"    // #f5e0dc
-#define glyph "\x1b[38;2;137;180;250m"    // #89b4fa
-#define arrow "\x1b[38;2;249;226;175m"    // #f9e2af
-#define text "\x1b[38;2;205;214;244m"     // #cdd6f4
 
 #define RED     "\x1b[31m"
 #define GREEN   "\x1b[32m"
@@ -13,6 +10,7 @@
 #define BLUE    "\x1b[34m"
 #define MAGENTA "\x1b[35m"
 #define CYAN    "\x1b[36m"
+#define WHITE   "\x1b[39m"
 #define RESET   "\x1b[0m"
 
 int main(){
@@ -36,12 +34,12 @@ int main(){
   int pkgs = getPkgs();
 
   // print system information with ascii
-  printf(ascii"\n\n  \\    /\\ "  glyph"  \uf2c0  " arrow"~>" text" %s\n", hostname);
-  printf(ascii"   )  ( ')"        glyph"  \ueaf8  " arrow"~>" text" %s\n", buildnum);
-  printf(ascii"  (  /  ) "        glyph"  \uf017  " arrow"~>" text" %s\n", uptime);
-  printf(ascii"   \\(__)| "       glyph"  \uf51e  " arrow"~>" text" %s\n", memusage);
+  printf(ascii"\n\n  \\    /\\ "  BLUE"  \uf2c0  " YELLOW"~>" WHITE" %s\n", hostname);
+  printf(ascii"   )  ( ')"        BLUE"  \ueaf8  " YELLOW"~>" WHITE" %s\n", buildnum);
+  printf(ascii"  (  /  ) "        BLUE"  \uf017  " YELLOW"~>" WHITE" %s\n", uptime);
+  printf(ascii"   \\(__)| "       BLUE"  \uf51e  " YELLOW"~>" WHITE" %s\n", memusage);
   if (pkgs != 0){
-    printf("          "           glyph"  \ueb29  " arrow"~>" text" %d (scoop)\n", pkgs);
+    printf("          "           BLUE"  \ueb29  " YELLOW"~>" WHITE" %d (scoop)\n", pkgs);
   }
   // print colored bar
   printf("\n     " RED "\u25CF   " GREEN "\u25CF   " YELLOW "\u25CF   " BLUE "\u25CF   " MAGENTA "\u25CF   " CYAN "\u25CF   " RESET "\u25CF   \n\n");
